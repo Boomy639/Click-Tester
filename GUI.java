6e55ccf6e0f610a5050efcfb883a75942fa7ff86
Boomy639 <74262738+Boomy639@users.noreply.github.com>
2021-06-19 15:16:28 +0100

    Add files via upload

---
+++ GUI.java
@@ -0,0 +1,67 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class GUI implements ActionListener {
+	
+	private int count = 0;
+	private JLabel label;
+	private JFrame frame;
+	private JPanel panel;
+	
+	public GUI() {
+		
+		
+		frame = new JFrame();
+		
+		JButton button = new JButton("Click Here");
+		 button.setBackground(new Color(59, 89, 182));
+	        button.setForeground(Color.WHITE);
+	        button.setFocusPainted(false);
+	        button.setFont(new Font("SANS_SERIF", Font.BOLD, 12));
+		button.addActionListener(this);
+		label = new JLabel("Number Of Clicks: 0");
+		
+		
+		
+		panel = new JPanel();
+		
+		panel.setBorder(BorderFactory.createEmptyBorder(70, 70, 70, 70));
+		panel.setLayout(new GridLayout(0, 1));
+		panel.add(button);
+		panel.add(label);
+		
+		
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Click Tester");
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new GUI();
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		count++;
+		label.setText("Number Of Clicks: " +count);
+		
+		
+		
+	}
+
+}
